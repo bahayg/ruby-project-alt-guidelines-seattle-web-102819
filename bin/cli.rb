2a0223c66f92f5dc7c517afea1acbd82db5ed1eb
bahayg <bahayg@gmail.com>
2019-11-14 13:46:55 -0800

    Add big text to welcoming message

--- bin/cli.rb
+++ bin/cli.rb
@@ -1,11 +1,22 @@
 require 'colorize'
+# require 'artii'
 require_relative '../config/environment'
+
+# arter = Artii::Base.new :font => 'slant'
+# arter.asciify
 
 def greet
   
     puts ""
     puts ""
     puts ""
+    puts " ____                     _                 _       _____   _               _ ".blue.bold   
+    puts "|  _ \\   _ __    ___     (_)   ___    ___  | |_    |  ___| (_)  _ __     __| |   ___   _ __ ".blue.bold 
+    puts "| |_) | | '__|  / _ \\    | |  / _ \\  / __| | __|   | |_    | | | '_ \\   / _` |  / _ \\ | '__|".blue.bold 
+    puts "|  __/  | |    | (_) |   | | |  __/ | (__  | |_    |  _|   | | | | | | | (_| | |  __/ | |".blue.bold    
+    puts "|_|     |_|     \\___/   _/ |  \\___|  \\___|  \\__|   |_|     |_| |_| |_|  \\__,_|  \\___| |_|".blue.bold    
+    puts "                       |__/".blue.bold                                                                 
+
     puts ""
     puts ""
     puts ""
@@ -31,7 +42,7 @@ def name
     puts ""
     name = gets.chomp
     puts ""
-    puts "#{name}, Please select a discipline from the following list by typing it to see professors studying in the same discipline:".blue.bold
+    puts "#{name}; please select a discipline from the following list by typing it to see professors studying in the same discipline:".blue.bold
     puts ""
 end
 
@@ -69,9 +80,21 @@ def find_by(discipline)
         puts ""
         puts ""
         puts ""
+        puts "If you want to see the results for a different discipline, please put your new choice below".blue.bold
         puts ""
       end
     end
 end
 
 find_by(discipline)
+
+def update_discipline(discipline)
+    discipline
+    
+
+end
+
+update_discipline(discipline)
+
+
+
